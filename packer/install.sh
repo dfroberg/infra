2803e342c0b460a05eeeb10a25894de8f193215c
felixschmitz <felix.schmitz@gmail.com>
2017-07-04 16:51:22 +1000

    N/A: updated key server address.

--- packer/install.sh
+++ packer/install.sh
@@ -26,7 +26,7 @@ export DEBIAN_FRONTEND=noninteractive
 sudo systemctl stop apt-daily
 
 # Setup docker repo
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' \
   > /etc/apt/sources.list.d/docker.list
 
